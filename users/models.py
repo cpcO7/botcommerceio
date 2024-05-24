95df98e58768c0e6c53505a612742d2ae2563365
wick <rakhmanov0410@gmail.com>
2024-05-24 18:07:01 +0500

    add Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db.models import Model, CharField, BooleanField, ForeignKey, CASCADE
 
-# Create your models here.
+
+class Shop(Model):
+    name = CharField(max_length=255)
+
+
+class CategoryHead(Model):
+    name = CharField(max_length=255)
+
+
+class Category(Model):
+    emoji_status = CharField(max_length=255)
+    name = CharField(max_length=255)
+    parent_category = ForeignKey('users.CategoryHead', CASCADE)
+    availability = BooleanField()
+    show_in_website = BooleanField()
