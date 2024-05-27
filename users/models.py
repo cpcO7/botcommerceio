79a694f80dedf53d876cdef316d36efea965c294
humoyun149 <ozoda17121965@gmail.com>
2024-05-27 15:22:06 +0500

    shop model and api added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.db import models
 from django.db.models import Model, CharField, BooleanField, ForeignKey, CASCADE
 
 
@@ -19,3 +21,42 @@ class Category(Model):
     parent_category = ForeignKey('users.CategoryHead', CASCADE)
     availability = BooleanField()
     show_in_website = BooleanField()
+
+
+class ShopCategory(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ShopProduct(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    weight = models.FloatField()
+    height = models.FloatField()
+    length = models.FloatField()
+    width = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Savat(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    price = models.FloatField()
+    paid = models.BooleanField(default=False)
